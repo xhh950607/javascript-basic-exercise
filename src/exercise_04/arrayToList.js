90b80a184c24f9b64bc5ce57a904d4f15ee8df66
xuhuihui <494655043@qq.com>
2020-07-21 20:41:08 +0800

    complete exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,16 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) throw Error('Creating list from undefined array');
+
+  if (array.length === 0) throw Error('Creating list from empty array');
+
+  const head = {};
+  array.reduce((pre, cur) => {
+    const node = { value: cur, next: null };
+    // eslint-disable-next-line no-param-reassign
+    pre.next = node;
+    return node;
+  }, head);
+  return head.next;
 }
