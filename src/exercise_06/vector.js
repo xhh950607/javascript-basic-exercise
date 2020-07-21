320f8b366dafa0f1c5ac84df53c0157aec4b8c74
xuhuihui <494655043@qq.com>
2020-07-21 21:22:48 +0800

    complete exercise_06

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -12,5 +12,18 @@ export default class Vector {
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() { return this._x; }
+
+  get y() { return this._y; }
+
+  static plus(v1, v2) { return new Vector(v1.x + v2.x, v1.y + v2.y); }
+
+  static minus(v1, v2) { return new Vector(v1.x - v2.x, v1.y - v2.y); }
+
+  distance() { return Math.sqrt(this.x * this.x + this.y * this.y); }
 }
